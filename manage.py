61de3b480759555ac21751a6eacd1f01ee844749
brianngeno <bkn.ngeno@gmail.com>
2018-09-13 23:26:00 +0300

    changes to structure

---
+++ manage.py
@@ -0,0 +1,24 @@
+from app import create_app
+from flask_script import Manager, Server
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manage.command
+def test():
+    '''
+    run test for the whole application
+    '''
+
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict (app=app)
+    pass
+
+if __name__ == '__main__':
+    manager.run()
