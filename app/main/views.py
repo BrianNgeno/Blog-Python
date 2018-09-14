6490715b190ec18169efc73a5d6a7a99b7021ad4
brianngeno <bkn.ngeno@gmail.com>
2018-09-14 06:41:16 +0300

    added a route to view the title and index page

---
+++ app/main/views.py
@@ -0,0 +1,11 @@
+from flask import render_template,redirect,url_for,abort,request
+from . import main
+
+@main.route('/')
+def index():
+    '''
+    view root page function that returns the index page
+    '''
+    title = 'Home - Welcome to The Best Blog Site Worldwide You Think of It We help share It.'
+
+    return render_template('index.html',title=title)
