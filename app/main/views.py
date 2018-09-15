cd9aca32677fe894d7cb26157d94ce7e6fbcb730
brianngeno <bkn.ngeno@gmail.com>
2018-09-15 14:07:07 +0300

    added a form class to auth and added class registration form

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template,redirect,url_for,abort,request
 from . import main
+from flask_login import login_required
 
 @main.route('/')
 def index():
@@ -9,3 +10,4 @@ def index():
     title = 'Home - Welcome to The Best Blog Site Worldwide You Think of It We help share It.'
 
     return render_template('index.html',title=title)
+
