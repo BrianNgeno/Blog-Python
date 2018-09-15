d01de001b6b200de90c1c3c4345586b5a1bb7ccc
brianngeno <bkn.ngeno@gmail.com>
2018-09-15 16:44:31 +0300

    updated the route to register user

--- app/auth/views.py
+++ app/auth/views.py
@@ -34,11 +34,11 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to Blog","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
-
 
 @auth.route('/logout')
 @login_required
