c0549e79c80ad18eb6a910ed6777f15e802bf589
brianngeno <bkn.ngeno@gmail.com>
2018-09-15 15:30:52 +0300

    added view function for profile in profile.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
-from flask import render_template,redirect,url_for,abort,request
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User
 from . import main
 from flask_login import login_required
 
@@ -12,3 +13,11 @@ def index():
 
     return render_template('index.html',title=title)
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
