937825e3da5cf6c2f11047bc9ac3b4d0e478daa9
brianngeno <bkn.ngeno@gmail.com>
2018-09-15 13:52:33 +0300

    added test for user

--- app/models.py
+++ app/models.py
@@ -11,6 +11,18 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
