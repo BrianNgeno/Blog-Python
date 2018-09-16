adebff85dc5ab2b98e5e4b1f5d2fc805b9bb76c7
brianngeno <bkn.ngeno@gmail.com>
2018-09-16 10:40:58 +0300

    working appropriately

--- config.py
+++ config.py
@@ -12,7 +12,8 @@ class Config():
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
+    SECRET_KEY=os.environ.get('SECRET_KEY') 
+  
     @staticmethod
     def init_app(app):
         pass
