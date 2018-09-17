5eaf0a1e87ca3baa5ef96eea331b6cee195c8ead
brianngeno <bkn.ngeno@gmail.com>
2018-09-17 09:42:01 +0300

    deploying to heroku

--- manage.py
+++ manage.py
@@ -3,11 +3,10 @@ from flask_script import Manager, Server
 from app.models import User,Role,Blog, Comments
 from  flask_migrate import Migrate, MigrateCommand
 
-app = create_app('development')
 '''
 creating app instance
 '''
-app= create_app('development')
+app= create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export MAIL_USERNAME=lukemark254@gmail.com
 export MAIL_PASSWORD=1234qwer,./
 export SECRET_KEY=1234
+export DATABASE_URL='postgres://fmnjtlfdxhxwqw:4d5fa1b37d49f52ca4ef81e4c09654d449de60db5646fe90d9d1c886b96e7bc9@ec2-54-83-27-165.compute-1.amazonaws.com:5432/d8urob8lnudtol'
 python3.6 manage.py server
