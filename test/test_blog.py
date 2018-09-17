9219e5914d98a9636ae8998707cf8aa58d7583eb
brianngeno <bkn.ngeno@gmail.com>
2018-09-17 22:13:13 +0300

    test run

--- test/test_blog.py
+++ test/test_blog.py
@@ -5,3 +5,6 @@ class UserModelTest(unittest.TestCase):
 
     def setUp(self):
         self.new_blog = Blog(category = 'Personal_Blog')
+
+if __name__ =='__main__':
+    unittest.main()
--- test/test_comment.py
+++ test/test_comment.py
@@ -5,3 +5,7 @@ class UserModelTest(unittest.TestCase):
 
     def setUp(self):
         self.new_comment =Comments( comment_name = 'lol')
+
+
+if __name__ =='__main__':
+    unittest.main()
